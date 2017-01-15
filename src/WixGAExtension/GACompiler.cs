fce024ca51df14472756504162c91ffbdf925b96
frederiksen <mrtn.frederiksen@gmail.com>
2017-01-15 17:58:10 +0100

    WixGAExtension: Add more implementation

--- src/WixGAExtension/GACompiler.cs
+++ src/WixGAExtension/GACompiler.cs
@@ -1,12 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Tools.WindowsInstallerXml;
+using System.Reflection;
+using System.Xml;
+using System.Xml.Schema;
 
 namespace WixGAExtension
 {
   public class GACompiler : CompilerExtension
   {
+    private XmlSchema schema;
+
+    public GACompiler()
+    {
+      this.schema =
+         CompilerExtension.LoadXmlSchemaHelper(
+            Assembly.GetExecutingAssembly(),
+            "WixGAExtension.ExtensionSchema.xsd");
+    }
+
+    public override XmlSchema Schema
+    {
+      get
+      {
+        return this.schema;
+      }
+    }
+
+    public override void ParseElement(
+       SourceLineNumberCollection sourceLineNumbers,
+       XmlElement parentElement,
+       XmlElement element,
+       params string[] contextValues)
+    {
+      switch (parentElement.LocalName)
+      {
+        case "Product":
+        case "Fragment":
+          switch (element.LocalName)
+          {
+            case "GoogleAnalytics":
+              this.ParseGoogleAnalytics(element);
+              break;
+            default:
+              this.Core.UnexpectedElement(
+                 parentElement,
+                 element);
+              break;
+          }
+          break;
+        default:
+          this.Core.UnexpectedElement(
+             parentElement,
+             element);
+          break;
+      }
+    }
+
+    private void ParseGoogleAnalytics(XmlNode node)
+    {
+      SourceLineNumberCollection sourceLineNumber =
+         Preprocessor.GetSourceLineNumbers(node);
+
+      string googleAnalyticsId = null;
+
+      foreach (XmlAttribute attribute in node.Attributes)
+      {
+        if (attribute.NamespaceURI.Length == 0 ||
+            attribute.NamespaceURI == this.schema.TargetNamespace)
+        {
+          switch (attribute.LocalName)
+          {
+            case "Id":
+              googleAnalyticsId = this.Core.GetAttributeIdentifierValue(
+                 sourceLineNumber,
+                 attribute);
+              break;
+            default:
+              this.Core.UnexpectedAttribute(
+                 sourceLineNumber,
+                 attribute);
+              break;
+          }
+        }
+        else
+        {
+          this.Core.UnsupportedExtensionAttribute(
+             sourceLineNumber,
+             attribute);
+        }
+      }
+
+      if (string.IsNullOrEmpty(googleAnalyticsId))
+      {
+        this.Core.OnMessage(
+           WixErrors.ExpectedAttribute(
+              sourceLineNumber,
+              node.Name,
+              "Id"));
+      }
+
+      if (!this.Core.EncounteredError)
+      {
+        Row superElementRow =
+             this.Core.CreateRow(
+                sourceLineNumber,
+                "GoogleAnalyticsTable");
+
+        superElementRow[0] = googleAnalyticsId;
+      }
+    }
+
   }
 }
---
+++ src/WixGAExtension/GAExtension.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Tools.WindowsInstallerXml;
+using System.Reflection;
+
+namespace WixGAExtension
+{
+  public class GAExtension : WixExtension
+  {
+    private CompilerExtension compilerExtension;
+
+    public override CompilerExtension CompilerExtension
+    {
+      get
+      {
+        if (this.compilerExtension == null)
+        {
+          this.compilerExtension =
+             new GACompiler();
+        }
+
+        return this.compilerExtension;
+      }
+    }
+
+    private TableDefinitionCollection tableDefinitions;
+
+    public override TableDefinitionCollection TableDefinitions
+    {
+      get
+      {
+        if (this.tableDefinitions == null)
+        {
+          this.tableDefinitions =
+             WixExtension.LoadTableDefinitionHelper(
+                Assembly.GetExecutingAssembly(),
+                "WixGAExtension.TableDefinitions.xml");
+        }
+
+        return this.tableDefinitions;
+      }
+    }
+  }
+}
