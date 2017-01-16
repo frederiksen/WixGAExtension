633a820496f95337af923b00cae5ba9c882ab8ed
frederiksen <mrtn.frederiksen@gmail.com>
2017-01-16 20:43:45 +0100

    Add CAs for a uninstallation

--- src/GALibraryCustomActions/CustomAction.cs
+++ src/GALibraryCustomActions/CustomAction.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections.Generic;
-using System.Text;
 using Microsoft.Deployment.WindowsInstaller;
 
 namespace GALibraryCustomActions
 {
   public class CustomActions
   {
+    #region Installation
+
     [CustomAction]
     public static ActionResult TrackInstallationImmediate(Session session)
     {
@@ -50,7 +51,7 @@ namespace GALibraryCustomActions
         {
           DisplayWarningMessage(
              session,
-             string.Format("{0} => {1}", datum.Key, datum.Value));
+             string.Format("Install {0} => {1}", datum.Key, datum.Value));
         }
 
         return ActionResult.Success;
@@ -62,11 +63,76 @@ namespace GALibraryCustomActions
       }
     }
 
+    #endregion
+
+    #region Uninstallation
+
+    [CustomAction]
+    public static ActionResult TrackUninstallationImmediate(Session session)
+    {
+      Database db = session.Database;
+
+      try
+      {
+        View view = db.OpenView("SELECT `Id`, `Tracking` FROM `GoogleAnalyticsTable`");
+        view.Execute();
+
+        CustomActionData data = new CustomActionData();
+
+        foreach (Record row in view)
+        {
+          data[row["Id"].ToString()] = row["Tracking"].ToString();
+        }
+
+        session["TrackUninstallationDeferred"] = data.ToString();
+
+        return ActionResult.Success;
+      }
+      catch (Exception ex)
+      {
+        session.Log(ex.Message);
+        return ActionResult.Failure;
+      }
+      finally
+      {
+        db.Close();
+      }
+    }
+
+    [CustomAction]
+    public static ActionResult TrackUninstallationDeferred(Session session)
+    {
+      try
+      {
+        CustomActionData data = session.CustomActionData;
+
+        foreach (KeyValuePair<string, string> datum in data)
+        {
+          DisplayWarningMessage(
+             session,
+             string.Format("Uninstall {0} => {1}", datum.Key, datum.Value));
+        }
+
+        return ActionResult.Success;
+      }
+      catch (Exception ex)
+      {
+        session.Log(ex.Message);
+        return ActionResult.Failure;
+      }
+    }
+
+    #endregion
+
+    #region Helpers
+
     private static void DisplayWarningMessage(Session session, string message)
     {
       Record record = new Record(0);
       record[0] = message;
       session.Message(InstallMessage.Warning, record);
     }
+
+    #endregion
   }
 }
